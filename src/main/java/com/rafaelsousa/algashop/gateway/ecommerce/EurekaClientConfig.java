03c583c0370ac1905affdb622e6a42dcfa0ce93c
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-07-20 14:58:12 -0300

    EMS-N-06-02.06 - Setting up routing with server-side service discovery and load balance.

---
+++ src/main/java/com/rafaelsousa/algashop/gateway/ecommerce/EurekaClientConfig.java
@@ -0,0 +1,8 @@
+package com.rafaelsousa.algashop.gateway.ecommerce;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {}
